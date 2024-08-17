447afe3cab868f0ea05ef3cb948130966704218a
sharathgitha627 <sharathgitha1627@gmail.com>
2024-08-17 10:39:30 +0530

    files added in the local repo

---
+++ src/main/java/com/jenkins/NormalSpringBootProject1Application.java
@@ -0,0 +1,17 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalSpringBootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormalSpringBootProject1Application.class, args);
+		
+		System.out.println("this is line number 1");
+		
+		System.out.println("this is line number 2");
+	}
+
+}
